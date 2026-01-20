968b943bb80c5b1fefb411f8d649003888e930e6
Siddharthan Dileep <aiy257585@login05.hpc.iitd.ac.in>
2026-01-20 13:54:35 +0530

    shell script to execute pipeline

--- run.sh
+++ run.sh
@@ -1,25 +1,10 @@
-#!/usr/bin/env bash
-#PBS -N anrf_check
-#PBS -P m3rg.spons
-#PBS -m bea
-#PBS -M $USER@scai.iitd.ac.in
-#PBS -l select=1:ncpus=1:ngpus=1:mem=10gb:centos=skylake
-#PBS -l walltime=00:30:00
-#PBS -q standard
-#################  Environment #################################
-echo "==============================="
-echo $PBS_JOBID
-cat $PBS_NODEFILE
-echo "==============================="
-cd $PBS_O_WORKDIR
+#!/bin/bash
+set -e
 
-##############################################################
+echo "running full pipeline - prepare dataset -> train -> infer -> eval"
 
-#######################################################################################
-source /home/scai/msr/aiy257585/.bashrc
-conda activate /home/scai/msr/aiy257585/envs/env1
-export PYTHONPATH=$(pwd)
-#python -u ./scripts/prepare_dataset.py
-#python -u ./scripts/train.py
-python -u ./scripts/infer.py
-python -u ./scripts/eval.py
+python ./scripts/prepare_dataset.py
+python ./scripts/train.py
+python ./scripts/infer.py
+python ./scripts/eval.py
+
