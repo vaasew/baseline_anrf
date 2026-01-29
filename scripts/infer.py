524abcaf16735f4601fdcac26900ec4240dd84fa
Siddharthan Dileep <siddharthan@Siddharthans-MacBook-Air.local>
2026-01-29 17:03:04 +0530

    pm emission proxy

--- scripts/infer.py
+++ scripts/infer.py
@@ -92,7 +92,10 @@ class DataLoaders(torch.utils.data.Dataset):
         self.arrs = {}
         for feat in self.all_features:
             path = os.path.join(cfg.paths.input_loc, f"{feat}.npy")
-            self.arrs[feat] = np.load(path, mmap_mode="r")
+            if feat!='PM25':
+                self.arrs[feat] = np.load(path, mmap_mode="r")
+            else:
+                self.arrs[feat]=np.zeroes((996,10,140,124))
 
         self.N = self.arrs[self.all_features[0]].shape[0]
 
